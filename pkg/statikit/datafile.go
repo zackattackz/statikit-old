1e3b94ef1566a5a68dd3eec9609f1624fe3b902b
Zachary Hanham <z.hanham00@gmail.com>
2022-05-14 16:36:23 -0400

    rename parsedata.go and create GetDataFilePath

---
+++ pkg/statikit/datafile.go
@@ -0,0 +1,111 @@
+package statikit
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+)
+
+type ParseDataFormat uint
+
+const (
+	JsonFormat ParseDataFormat = iota
+	TomlFormat
+
+	DataFileName = "renderData"
+)
+
+var extToFormat = map[string]ParseDataFormat{
+	".json": JsonFormat,
+	".toml": TomlFormat,
+}
+
+type MoreThanOneError struct {
+	amount uint
+}
+
+func (e MoreThanOneError) Error() string {
+	return fmt.Sprintf("too many data files: %v", e.amount)
+}
+
+func (e MoreThanOneError) Is(target error) bool {
+	targetCast, ok := target.(MoreThanOneError)
+	if !ok {
+		return false
+	}
+	return targetCast.amount == e.amount
+}
+
+func moreThanOneError(amount uint) MoreThanOneError {
+	return MoreThanOneError{amount: amount}
+}
+
+var (
+	ErrDataFileNotExist error = errors.New(fs.ErrNotExist.Error())
+)
+
+type ParseDataArgs struct {
+	r      io.Reader
+	format ParseDataFormat
+}
+
+/*
+Searches for a single valid data file in path `root`
+
+Returns:
+	(string, ParseDataFormat, nil) The full path to the single data file and its format
+	(_, _, MoreThanOneErr) If more than one data file exists
+	(_, _, ErrDataFileNotExist) If no data files exist
+	(_, _, error) Any generic error from os calls
+*/
+func GetDataFilePath(root string) (resPath string, f ParseDataFormat, resErr error) {
+	// For each valid extention, ext,
+	// If the file at path "root/p.ext" exists and is regular,
+	// return the path and it's associated format
+	// if no valid file is found return fs.ErrNotExist
+	count := uint(0)
+	for ext := range extToFormat {
+		p := filepath.Join(root, DataFileName+ext)
+		s, err := os.Stat(p)
+		if errors.Is(err, fs.ErrNotExist) {
+			continue
+		} else if err != nil {
+			resErr = err
+			return
+		} else if !s.Mode().IsRegular() {
+			continue
+		} else {
+			resPath = p
+			f = extToFormat[ext]
+			count += 1
+		}
+	}
+	if count == 0 {
+		resErr = ErrDataFileNotExist
+	} else if count > 1 {
+		resErr = moreThanOneError(count)
+	}
+	return
+}
+
+func ParseDataFile(a ParseDataArgs) (any, error) {
+	result := make(map[string]interface{})
+	switch a.format {
+	case JsonFormat:
+		d := json.NewDecoder(a.r)
+		d.Decode(&result)
+		return result, nil
+	case TomlFormat:
+		d := toml.NewDecoder(a.r)
+		_, err := d.Decode(&result)
+		return result, err
+	default:
+		return nil, errors.New("invalid format")
+	}
+}
---
+++ pkg/statikit/datafile_test.go
@@ -0,0 +1,139 @@
+package statikit
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+type runTestArgs struct {
+	input    string
+	expected map[string]interface{}
+}
+
+func runTest(a runTestArgs, format ParseDataFormat) error {
+	r := strings.NewReader(a.input)
+	actual, err := ParseDataFile(ParseDataArgs{r: r, format: format})
+	if err != nil {
+		return err
+	}
+	actualMap := actual.(map[string]interface{})
+	if !reflect.DeepEqual(actualMap, a.expected) {
+		return fmt.Errorf("expected: \"%v\", actual: \"%v\"", a.expected, actualMap)
+	}
+	return nil
+}
+
+func TestParseData(t *testing.T) {
+	tomlTests := []runTestArgs{
+		{
+			input:    `Test = "hello"`,
+			expected: map[string]interface{}{"Test": "hello"},
+		},
+		{
+			input:    "One = 1\nTwo = 2",
+			expected: map[string]interface{}{"One": int64(1), "Two": int64(2)},
+		},
+	}
+
+	jsonTests := []runTestArgs{
+		{
+			input:    `{"Test": "hello"}`,
+			expected: map[string]interface{}{"Test": "hello"},
+		},
+		{
+			input:    `{"One": 1, "Two": 2}`,
+			expected: map[string]interface{}{"One": float64(1), "Two": float64(2)},
+		},
+	}
+
+	for _, jsonTest := range jsonTests {
+		err := runTest(jsonTest, JsonFormat)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for _, tomlTest := range tomlTests {
+		err := runTest(tomlTest, TomlFormat)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+type expectedResults struct {
+	p   string
+	f   ParseDataFormat
+	err error
+}
+
+func TestGetDataFilePath(t *testing.T) {
+
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("error on os.Getwd(): %s", err)
+	}
+	testcasesPath := filepath.Join(wd, "testcases", "datafile")
+	fmt.Println(testcasesPath)
+	in := filepath.Join(testcasesPath, "in")
+
+	expectedResults := map[string]expectedResults{
+		"one": {
+			p:   filepath.Join(in, "one", DataFileName+".json"),
+			f:   JsonFormat,
+			err: nil,
+		},
+		"two": {
+			p:   filepath.Join(in, "two", DataFileName+".toml"),
+			f:   TomlFormat,
+			err: nil,
+		},
+		"three": {
+			p:   "",
+			f:   0,
+			err: ErrDataFileNotExist,
+		},
+		"four": {
+			p:   "",
+			f:   0,
+			err: ErrDataFileNotExist,
+		},
+		"five": {
+			p:   "",
+			f:   0,
+			err: moreThanOneError(2),
+		},
+	}
+
+	d, err := os.ReadDir(in)
+	if err != nil {
+		t.Fatalf("error on ReadDir(\"%s\"): %s", in, err)
+	}
+
+	for _, e := range d {
+		if !e.IsDir() {
+			t.Fatalf("entry is not a directory: %s", e.Name())
+		}
+
+		in := filepath.Join(in, e.Name())
+
+		p, f, err := GetDataFilePath(in)
+		expected := expectedResults[e.Name()]
+		if err != nil {
+			if !errors.Is(expected.err, err) {
+				t.Fatalf("expected.err = %v, actual err = %v", expected.err, err)
+			}
+		} else {
+			if expected.p != p {
+				t.Fatalf("expected.p = %v, actual p = %v", expected.p, p)
+			} else if expected.f != f {
+				t.Fatalf("expected.f = %v, actual f = %v", expected.f, f)
+			}
+		}
+	}
+}
